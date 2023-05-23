1c570dcc337b69f3a0c7188f62eace5de973a327
Marc Bitterli <bimarc@student.ethz.ch>
2023-05-23 14:08:23 +0200

    phones and thu/fr

--- src/app/data/people.ts
+++ src/app/data/people.ts
@@ -17,25 +17,26 @@ const nonDoctorsArray: Person[] = [
 ];
 
 const doctorsArray: Person[] = [
-	{ name: "David Goerdeler", title: "AA", phone: 0, id: "goerdeler" },
-	{ name: "Dr. Abo Youssef", title: "OA", phone: 0, id: "abo_youssef" },
-	{ name: "Prof. Dr. Platz", title: "CA", phone: 0, id: "platz" },
-	{ name: "Dr. L. Lance", title: "AA", phone: 0, id: "lance" },
-	{ name: "Dr. Leimbacher", title: "OA", phone: 0, id: "leimbacher" },
-	{ name: "O. Klee", title: "AA", phone: 0, id: "klee" },
+	{ name: "David Goerdeler", title: "AA", phone: 64134, id: "goerdeler" },
+	{ name: "Dr. Abo Youssef", title: "OA", phone: 64727, id: "abo_youssef" },
+	{ name: "Prof. Dr. Platz", title: "CA", phone: 64700, id: "platz" },
+	{ name: "Dr. L. Lance", title: "AA", phone: 64132, id: "lance" },
+	{ name: "Dr. Leimbacher", title: "OA", phone: 73133, id: "leimbacher" },
+	{ name: "O. Klee", title: "AA", phone: 64145, id: "klee" },
 	{ name: "PD Dr. Dietrich", title: "CA?", phone: 0, id: "dietrich" },
 	{ name: "Dr. Kilgus", title: "LA", phone: 64706, id: "kilgus" },
-	{ name: "Dr. Johannson", title: "OA", phone: 0, id: "johannson" },
+	{ name: "Dr. Johannson", title: "OA", phone: 64734, id: "johannson" },
 	{ name: "P. Iliev", title: "AA", phone: 64135, id: "iliev" },
-	{ name: "Dr. Rosenkranz", title: "LA", phone: 0, id: "rosenkranz" },
-	{ name: "Dr. Spagna", title: "OA", phone: 0, id: "spagna" },
+	{ name: "Dr. Rosenkranz", title: "LA", phone: 64704, id: "rosenkranz" },
+	{ name: "Dr. Spagna", title: "OA", phone: 64731, id: "spagna" },
 	{ name: "Dr. Müller", title: "LA", phone: 64710, id: "müller" },
 	{ name: "Dr. Löwe", title: "OA", phone: 64724, id: "löwe" },
 	{ name: "Jana Siegfried", title: "AA", phone: 64105, id: "siegfried" },
-	{ name: "Dr. Aloji", title: "OA", phone: 0, id: "aloji" },
+	{ name: "Dr. Aloji", title: "OA", phone: 64732, id: "aloji" },
 	{ name: "Julia Leipner", title: "AA", phone: 64101, id: "leipner" },
-	{ name: "Dr. Bauchspiess", title: "LA", phone: 0, id: "bauchspiess" },
-	{ name: "Berfin Caliskan", title: "AA", phone: 0, id: "caliskan" },
+	{ name: "Dr. Bauchspiess", title: "LA", phone: 64719, id: "bauchspiess" },
+	{ name: "Berfin Caliskan", title: "AA", phone: 64118, id: "caliskan" },
+	{ name: "Dr. Sproedt", title: "AA", phone: 72960, id: "sproedt" },
 ];
 
 export const doctors = Object.fromEntries(
